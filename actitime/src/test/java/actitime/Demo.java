89a1d9e5fd3db47192b1b50f5b8a2e52055101c8
happy <happy@Pritish>
2020-07-06 02:30:05 +0530

    Commit 1

---
+++ actitime/src/test/java/actitime/Demo.java
@@ -0,0 +1,16 @@
+package actitime;
+
+import org.testng.Reporter;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void run() {
+		
+		Reporter.log("Test Is Ok",true);
+		
+	}
+
+}
